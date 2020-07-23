5ef1d01caf79bbdcce2ff675222b79537f305389
CodingLife99 <yesh.jojo444@gmail.com>
2020-07-23 23:14:03 +0530

    Initial commit

--- facemask_detector/wsgi.py
+++ facemask_detector/wsgi.py
@@ -11,5 +11,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facemask_detector.settings')
+
 application = get_wsgi_application()
+
 application = DjangoWhiteNoise(application)
